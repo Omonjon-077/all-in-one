a3547f9a7f78d004285457fb49017e10793026d5
Omonjon <omonjons077@gmail.com>
2023-10-03 16:14:22 +0500

    Banner swiper added and modified

--- Samsung RU/assets/script/main.js
+++ Samsung RU/assets/script/main.js
@@ -25,156 +25,15 @@ if ($("#myHeader").length) {
     }
 }
 
-/*=============== PARTNERS | SWIPER ===============*/
-if ($(".partnerSwiper").length) {
-    let swiper = new Swiper(".partnerSwiper", {
-        slidesPerView: 6,
-        spaceBetween: 30,
+/*=============== BANNER | SWIPER ===============*/
+if ($(".bannerSwiper").length) {
+    let swiper = new Swiper(".bannerSwiper", {
         loop: true,
         grabCursor: true,
         autoplay: true,
-        breakpoints: {
-            // when window width is >= 280px
-            280: {
-                slidesPerView: 1,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 575px
-            575: {
-                slidesPerView: 2,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 768px
-            768: {
-                slidesPerView: 3,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 992px
-            992: {
-                slidesPerView: 4,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 1200px
-            1200: {
-                slidesPerView: 5,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 1440px
-            1440: {
-                slidesPerView: 6,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-        }
     });
 }
 
-
-/*=============== TESTIMONIAL | SWIPER ===============*/
-if ($(".testimonial").length) {
-    let swiper = new Swiper(".testimonial", {
-        slidesPerView: 6,
-        spaceBetween: 30,
-        loop: true,
-        grabCursor: true,
-        autoplay: true,
-        breakpoints: {
-            // when window width is >= 280px
-            280: {
-                slidesPerView: 1,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 575px
-            575: {
-                slidesPerView: 2,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 768px
-            768: {
-                slidesPerView: 3,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 992px
-            992: {
-                slidesPerView: 4,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 1200px
-            1200: {
-                slidesPerView: 5,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 1440px
-            1440: {
-                slidesPerView: 6,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-        }
-    });
-}
-
-/*=============== NEWS | SWIPER ===============*/
-if ($(".news-section").length) {
-    let swiper = new Swiper(".news-section", {
-        grabCursor: true,
-        slidesPerView: 3,
-        // spaceBetween: 10,
-        loop: true,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-            dynamicBullets: true,
-        },
-        breakpoints: {
-            // when window width is >= 280px
-            280: {
-                slidesPerView: 1,
-                spaceBetween: 15,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 575px
-            575: {
-                slidesPerView: 1,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 768px
-            768: {
-                slidesPerView: 2,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 992px
-            992: {
-                slidesPerView: 2,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-            // when window width is >= 1200px
-            1200: {
-                slidesPerView: 3,
-                // spaceBetween: 0,
-                // slideToClickedSlide: true,
-            },
-        },
-        keyboard: true,
-    });
-}
 /*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up')
